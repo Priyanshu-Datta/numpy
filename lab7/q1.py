37c7a57362e7cfc15499509682474c8d4b5cd35d
Priyanshu-Datta <priyanshudatta80@gmail.com>
2026-02-20 00:13:56 +0530

    modified added

--- lab7/q1.py
+++ lab7/q1.py
@@ -5,6 +5,13 @@ arr = np.arange(1, 13)
 
 
 print("Array:", arr)
-
-print("Data Type:", arr.dtype)
+print("Data type:", arr.dtype)
 print("Shape:", arr.shape)
+print("Size:", arr.size)
+
+
+reshaped_arr = arr.reshape(3, 4)
+
+
+print("Reshaped Array (3x4):")
+print(reshaped_arr)
